e9d464b4ea6197e6b219752b303a0b0add4d67f2
baghdadraza <71591172+baghdadraza@users.noreply.github.com>
2024-09-10 23:47:44 +0500

    Initial commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="">
+    <q-tabs class="bg-primary text-white" v-model="tab">
+      <q-tab name="attendance" label="Attendance" />
+      <q-tab name="homework" label="Homework" />
+    </q-tabs>
+    <q-tab-panels v-model="tab" swipeable animated>
+      <q-tab-panel name="attendance">
+        <div class="text-h6">Attendance</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+
+      <q-tab-panel name="homework">
+        <div class="text-h6">Homework</div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+      </q-tab-panel>
+    </q-tab-panels>
+  </q-page>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const tab = ref('attendance');
+</script>
---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,135 @@
+<template>
+  <q-page class="column">
+    <div class="q-ma-md" style="max-width: 400px">
+      <q-form @submit="onSubmit" @reset="onReset" class="q-gutter-md">
+        <q-input filled v-model="username" label="Username" />
+        <q-input filled v-model="password" label="Password" type="password" />
+
+        <!-- <q-toggle v-model="accept" label="I accept the license and terms" /> -->
+
+        <div>
+          <q-btn label="Login" type="submit" color="primary" />
+          <!-- <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" /> -->
+        </div>
+      </q-form>
+
+      <q-form @submit="atten" @reset="onReset" class="q-gutter-md">
+        <div>
+          <q-btn label="Mark Attendance" type="submit" color="primary" />
+          <!-- <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" /> -->
+        </div>
+      </q-form>
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { useQuasar } from 'quasar';
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const $q = useQuasar();
+
+    const username = ref('baghdad__raza');
+    const password = ref('Rz4sjX86RCgjHPA');
+
+    return {
+      username,
+      password,
+
+      onSubmit() {
+        const myHeaders = new Headers();
+        myHeaders.append(
+          'Accept',
+          'application/json, text/javascript, */*; q=0.01'
+        );
+        myHeaders.append('Accept-Language', 'en-US,en;q=0.9');
+        // myHeaders.append('Cache-Control', 'no-cache');
+        myHeaders.append('Connection', 'keep-alive');
+        myHeaders.append(
+          'Content-Type',
+          'application/x-www-form-urlencoded; charset=UTF-8'
+        );
+        myHeaders.append('DNT', '1');
+        myHeaders.append('Origin', 'https://eureka.ilmversity.net');
+        myHeaders.append(
+          'Referer',
+          'https://eureka.ilmversity.net/school/site/login'
+        );
+        myHeaders.append('Sec-Fetch-Dest', 'empty');
+        myHeaders.append('Sec-Fetch-Mode', 'cors');
+        myHeaders.append('Sec-Fetch-Site', 'same-origin');
+        myHeaders.append('X-Requested-With', 'XMLHttpRequest');
+
+        const raw = `LoginForm%5Busername%5D=${username.value}&LoginForm%5Bpassword%5D=${password.value}&LoginForm%5BrememberMe%5D=1`;
+
+        const requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          mode: 'cors',
+          //credentials: 'same-origin',
+          //credentials: 'include',
+        };
+
+        const url = 'https://eureka.ilmversity.net/school/site/login';
+        fetch(new Request(url, { redirect: 'manual' }), requestOptions)
+          .then((response) => {
+            return response;
+          })
+          .then((result) => console.log(result))
+          .catch((error) => console.error(error));
+      },
+
+      onReset() {
+        username.value = null;
+        password.value = null;
+      },
+
+      atten() {
+        const myHeaders = new Headers();
+        myHeaders.append(
+          'Accept',
+          'application/json, text/javascript, */*; q=0.01'
+        );
+        myHeaders.append('Accept-Language', 'en-US,en;q=0.9,ur;q=0.8');
+        myHeaders.append('Connection', 'keep-alive');
+        myHeaders.append(
+          'Content-Type',
+          'application/x-www-form-urlencoded; charset=UTF-8'
+        );
+        myHeaders.append('DNT', '1');
+        myHeaders.append('Origin', 'https://eureka.ilmversity.net');
+        myHeaders.append(
+          'Referer',
+          'https://eureka.ilmversity.net/school/attendance/student'
+        );
+        myHeaders.append('Sec-Fetch-Dest', 'empty');
+        myHeaders.append('Sec-Fetch-Mode', 'cors');
+        myHeaders.append('Sec-Fetch-Site', 'same-origin');
+        myHeaders.append('X-Requested-With', 'XMLHttpRequest');
+
+        const raw = 'attenVal=1&id=option_4478&dateVal=2024-09-01';
+
+        const requestOptions = {
+          method: 'POST',
+          headers: myHeaders,
+          body: raw,
+          mode: 'cors',
+          redirect: 'follow',
+        };
+
+        const url =
+          'https://eureka.ilmversity.net/school/attendance/singleattendance';
+        fetch(new Request(url, { redirect: 'manual' }), requestOptions)
+          .then((response) => {
+            console.log(response);
+          })
+          .then((result) => console.log(result))
+          .catch((error) => console.error(error));
+      },
+    };
+  },
+};
+</script>
