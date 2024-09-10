98e45c200b95fa047a48fd99df91b94a9a970038
baghdadraza <baghdadraza1998@gmail.com>
2024-09-11 00:00:59 +0500

    create login page

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -38,48 +38,76 @@ export default {
       username,
       password,
 
-      onSubmit() {
-        const myHeaders = new Headers();
-        myHeaders.append(
-          'Accept',
-          'application/json, text/javascript, */*; q=0.01'
-        );
-        myHeaders.append('Accept-Language', 'en-US,en;q=0.9');
-        // myHeaders.append('Cache-Control', 'no-cache');
-        myHeaders.append('Connection', 'keep-alive');
-        myHeaders.append(
-          'Content-Type',
-          'application/x-www-form-urlencoded; charset=UTF-8'
-        );
-        myHeaders.append('DNT', '1');
-        myHeaders.append('Origin', 'https://eureka.ilmversity.net');
-        myHeaders.append(
-          'Referer',
-          'https://eureka.ilmversity.net/school/site/login'
-        );
-        myHeaders.append('Sec-Fetch-Dest', 'empty');
-        myHeaders.append('Sec-Fetch-Mode', 'cors');
-        myHeaders.append('Sec-Fetch-Site', 'same-origin');
-        myHeaders.append('X-Requested-With', 'XMLHttpRequest');
+      async onSubmit() {
+        try {
+          const response = await fetch(
+            'https://flask-hello-world-ktjt.vercel.app/login',
+            {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ username, password }),
+            }
+          );
 
-        const raw = `LoginForm%5Busername%5D=${username.value}&LoginForm%5Bpassword%5D=${password.value}&LoginForm%5BrememberMe%5D=1`;
+          if (!response.ok) {
+            throw new Error('Login failed');
+          }
 
-        const requestOptions = {
-          method: 'POST',
-          headers: myHeaders,
-          body: raw,
-          mode: 'cors',
-          //credentials: 'same-origin',
-          //credentials: 'include',
-        };
+          const data = await response.json();
+          const token = data.token;
 
-        const url = 'https://eureka.ilmversity.net/school/site/login';
-        fetch(new Request(url, { redirect: 'manual' }), requestOptions)
-          .then((response) => {
-            return response;
-          })
-          .then((result) => console.log(result))
-          .catch((error) => console.error(error));
+          // Store the token in local storage
+          localStorage.setItem('token', token);
+          // Redirect to the protected page
+          // window.location.href = '/protected-page';
+        } catch (error) {
+          // messageElement.textContent = 'Login failed: ' + error.message;
+          console.log(error);
+        }
+
+        // const myHeaders = new Headers();
+        // myHeaders.append(
+        //   'Accept',
+        //   'application/json, text/javascript, */*; q=0.01'
+        // );
+        // myHeaders.append('Accept-Language', 'en-US,en;q=0.9');
+        // // myHeaders.append('Cache-Control', 'no-cache');
+        // myHeaders.append('Connection', 'keep-alive');
+        // myHeaders.append(
+        //   'Content-Type',
+        //   'application/x-www-form-urlencoded; charset=UTF-8'
+        // );
+        // myHeaders.append('DNT', '1');
+        // myHeaders.append('Origin', 'https://eureka.ilmversity.net');
+        // myHeaders.append(
+        //   'Referer',
+        //   'https://eureka.ilmversity.net/school/site/login'
+        // );
+        // myHeaders.append('Sec-Fetch-Dest', 'empty');
+        // myHeaders.append('Sec-Fetch-Mode', 'cors');
+        // myHeaders.append('Sec-Fetch-Site', 'same-origin');
+        // myHeaders.append('X-Requested-With', 'XMLHttpRequest');
+
+        // const raw = `LoginForm%5Busername%5D=${username.value}&LoginForm%5Bpassword%5D=${password.value}&LoginForm%5BrememberMe%5D=1`;
+
+        // const requestOptions = {
+        //   method: 'POST',
+        //   headers: myHeaders,
+        //   body: raw,
+        //   mode: 'cors',
+        //   //credentials: 'same-origin',
+        //   //credentials: 'include',
+        // };
+
+        // const url = 'https://eureka.ilmversity.net/school/site/login';
+        // fetch(new Request(url, { redirect: 'manual' }), requestOptions)
+        //   .then((response) => {
+        //     return response;
+        //   })
+        //   .then((result) => console.log(result))
+        //   .catch((error) => console.error(error));
       },
 
       onReset() {
